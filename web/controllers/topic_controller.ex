933b9bc2137ba8efc29125d7df5ea677e2958f84
markchernov <mark.chernov@gmail.com>
2017-03-26 22:15:52 -0400

    topic routes and actions

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,11 +1,55 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller
+    alias Discuss.Topic
+
+    def index(conn, _params) do
+        topics = Repo.all(Topic)
+        render conn, "index.html", topics: topics
+    end    
 
     def new(conn, params) do
-        IO.puts "++++"
-        IO.inspect conn
-        IO.puts "++++"
+        IO.puts "+++++++++++++++++++++++++++++++++++++++++++++++++++"
         IO.inspect params
-        IO.puts "++++"
+        IO.puts "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+        changeset = Topic.changeset(%Topic{}, %{})
+        render conn, "new.html", changeset: changeset
+
     end
+
+    def create(conn, %{"topic" => topic}) do
+        IO.puts "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+        IO.inspect topic
+        IO.puts "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            {:ok, _topic} -> 
+                conn
+                |> put_flash(:info, "Topic Created")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> 
+                render conn, "new.html", changeset: changeset
+        end
+    end
+
+    def edit(conn, %{"id" => topic_id}) do
+        topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(topic)
+        render conn, "edit.html", changeset: changeset, topic: topic 
+    end  
+
+    def update(conn, %{"id" => topic_id, "topic" => topic})  do
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            # {:ok, _topic} -> 
+            #     conn
+            #     |> put_flash(:info, "Topic Created")
+            #     |> redirect(to: topic_path(conn, :index))
+            # {:error, changeset} -> 
+            #     render conn, "new.html", changeset: changeset
+        end
+    end    
 end    
