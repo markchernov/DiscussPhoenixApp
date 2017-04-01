5bb0efe31ef3b77e12346d3f1f9be7bc5ea53a5e
markchernov <mark.chernov@gmail.com>
2017-03-31 20:02:31 -0400

    linked topics to users

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -21,11 +22,13 @@ defmodule Discuss.Router do
     # get "/topics/:id/edit", TopicController, :edit
     # put "/topics/:id", TopicController, :update
     # delete "/topics/:id", TopicController, :delete
+    # get "/topics/:id", TopicController, :show                      
     resources "/", TopicController
   end
 
   scope "/auth", Discuss do
     pipe_through :browser
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
   end  
